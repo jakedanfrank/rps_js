fc97a68ddda3cbc7b5d8befd39f7cccfdb4edf11
Jake Frank <jacob423frank@gmail.com>
2019-09-05 09:16:54 -0600

    choices

--- index.js
+++ index.js
@@ -6,14 +6,17 @@ var userChoice;
 
 rock.addEventListener( "click", function(){
   userChoice = "rock";
+  document.getElementById("selection").innerHTML = "--- ROCK ---"
   compare(userChoice)
 });
 paper.addEventListener( "click", function(){
   userChoice = "paper";
+  document.getElementById("selection").innerHTML = "--- PAPER ---"
   compare(userChoice)
 });
 scissors.addEventListener( "click", function(){
   userChoice = "scissors";
+    document.getElementById("selection").innerHTML = "--- SCISSORS ---"
   compare(userChoice)
 });
 
@@ -21,17 +24,18 @@ function compare(userChoice){
   var battle = ["rock", "paper", "scissors"];
   compChoice = battle[Math.floor(Math.random()*battle.length)];
   if (userChoice === compChoice){
-        document.getElementById("outcome").innerHTML = "TIE";
+        document.getElementById("outcome").innerHTML = `IT'S A TIE! You chose ${userChoice}, Computer chose ${compChoice}`;
       }
      else if (
       userChoice === "rock" && compChoice === "scissors" ||
       userChoice === "paper" && compChoice === "rock" ||
       userChoice === "scissors" && compChoice === "paper"
       ){ 
-        document.getElementById("outcome").innerHTML = "WINNER";
+        document.getElementById("outcome").innerHTML = `YOU WIN! You chose ${userChoice}, Computer chose ${compChoice}`;
+
       }
      else {
-        document.getElementById("outcome").innerHTML = "YOU LOSE!";
+        document.getElementById("outcome").innerHTML = `YOU LOSE! You chose ${userChoice}, Computer chose ${compChoice}`;
     }
   }
 
